ab8f2d3ca8dc45d697cd33cd3a0617f95eff20de
YJangir49 <y.lkjangir49@gmail.com>
2022-02-28 16:27:28 +0530

    Auth Provider Added

--- src/App.js
+++ src/App.js
@@ -5,9 +5,11 @@ import UserList from './Users';
 import PostList, { PostEdit, PostCreate } from './components/Posts';
 import { PostAddSharp, GroupSharp } from '@material-ui/icons';
 import Dashboard from './components/Dashboard';
+import authProvider from "./authProvider";
 
 const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
-const App = () => (<Admin dashboard={Dashboard} dataProvider={dataProvider} >
+
+const App = () => (<Admin authProvider={authProvider} dashboard={Dashboard} dataProvider={dataProvider} >
     <Resource icon={GroupSharp} name="users" list={UserList}/>
     <Resource icon={PostAddSharp} name="posts" list={PostList} edit={PostEdit} create={PostCreate}/>
 </Admin>);
